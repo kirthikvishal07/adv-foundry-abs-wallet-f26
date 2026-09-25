c9bb3e156d7dbda11bb170fc2d29a1192d815ecd
kirthikvishal07 <kirthikvishal07@gmail.com>
2026-09-25 03:39:21 +0000

    day 7

--- script/SendPackedUserOp.s.sol
+++ script/SendPackedUserOp.s.sol
@@ -1,62 +1,78 @@
-//SPDX-License-Identifier: MIT
+// //SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.28;
+// pragma solidity ^0.8.28;
 
-import {Script} from "forge-std/Script.sol";
-import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
-import {HelperConfig} from "./HelperConfig.s.sol";
-import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
-import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+// import {Script} from "forge-std/Script.sol";
+// import {PackedUserOperation} from "lib/account-abstraction/contracts/interfaces/PackedUserOperation.sol";
+// import {HelperConfig} from "./HelperConfig.s.sol";
+// import {IEntryPoint} from "lib/account-abstraction/contracts/interfaces/IEntryPoint.sol";
+// import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+// import {IERC20} from "lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+// import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+// import {DeployMinimal} from "./DeployMinimal.s.sol";
 
-contract SendPackedUserOp is Script {
-    using MessageHashUtils for bytes32;
-    function run() public {}
+// contract SendPackedUserOp is Script {
+//     using MessageHashUtils for bytes32;
+//     function run() public {
+//         (HelperConfig helperConfig, MinimalAccount minimalAccount) = new DeployMinimal().deployMinimalAccount();
+//         HelperConfig.NetworkConfig memory config = helperConfig.getConfig();
+//         address dest = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+//         uint256 value = 0;
+//         bytes memory functionData = abi.encodeWithSelector(IERC20.approve.selector, 0xbf2B63cCeE4d24F63c138b6a9b82ef14f6793779, 1e18);
+//         bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector, dest, value, functionData);
+//         PackedUserOperation memory userOp = generatedSignedUserOperation(executeCallData, config, address(minimalAccount));
+//         PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+//         ops[0] = userOp;
 
-    function generatedSignedUserOperation(bytes memory callData,HelperConfig.NetworkConfig memory config,address minimalAccount)
-        public view
-        returns (PackedUserOperation memory)
-    {
-        //generate unsigned data
-        uint256 nonce = vm.getNonce(minimalAccount)-1;
-        PackedUserOperation memory unsignedUserOp = _generateUnsignedUserOperation(callData, minimalAccount, nonce);
-        //get the userOp hash
-        bytes32 userOpHash = IEntryPoint(config.entryPoint).getUserOpHash(unsignedUserOp);
-        bytes32 digest = userOpHash.toEthSignedMessageHash();
-        //sign it 
-        uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
-        uint8 v;
-        bytes32 r;
-        bytes32 s;
-        if (block.chainid == 31337) {
-            (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
-        } else {
-            // It'll use whatever account is unlocked, BUT there is no unlocked account for tests
-            (v, r, s) = vm.sign(config.account, digest);
-        }
-        unsignedUserOp.signature = abi.encodePacked(r,s,v);
-        return unsignedUserOp;
-    }
+//         vm.startBroadcast();
+        
+//     }
 
-    function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
-        internal
-        pure
-        returns (PackedUserOperation memory)
-    {
-        uint128 verificationGasLimit = 16777216;
-        uint128 callGasLimit = verificationGasLimit;
-        uint128 maxPriorityFeePerGas = 256;
-        uint128 maxFeePerGas = maxPriorityFeePerGas;
+//     function generatedSignedUserOperation(bytes memory callData,HelperConfig.NetworkConfig memory config,address minimalAccount)
+//         public view
+//         returns (PackedUserOperation memory)
+//     {
+//         //generate unsigned data
+//         uint256 nonce = vm.getNonce(minimalAccount)-1;
+//         PackedUserOperation memory unsignedUserOp = _generateUnsignedUserOperation(callData, minimalAccount, nonce);
+//         //get the userOp hash
+//         bytes32 userOpHash = IEntryPoint(config.entryPoint).getUserOpHash(unsignedUserOp);
+//         bytes32 digest = userOpHash.toEthSignedMessageHash();
+//         //sign it 
+//         uint256 ANVIL_DEFAULT_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+//         uint8 v;
+//         bytes32 r;
+//         bytes32 s;
+//         if (block.chainid == 31337) {
+//             (v, r, s) = vm.sign(ANVIL_DEFAULT_KEY, digest);
+//         } else {
+//             // It'll use whatever account is unlocked, BUT there is no unlocked account for tests
+//             (v, r, s) = vm.sign(config.account, digest);
+//         }
+//         unsignedUserOp.signature = abi.encodePacked(r,s,v);
+//         return unsignedUserOp;
+//     }
 
-        return PackedUserOperation({
-            sender: sender,
-            nonce: nonce,
-            initCode: hex"",
-            callData: callData,
-            accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
-            preVerificationGas: verificationGasLimit,
-            gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
-            paymasterAndData: hex"",
-            signature: hex""
-        });
-    }
-}
+//     function _generateUnsignedUserOperation(bytes memory callData, address sender, uint256 nonce)
+//         internal
+//         pure
+//         returns (PackedUserOperation memory)
+//     {
+//         uint128 verificationGasLimit = 16777216;
+//         uint128 callGasLimit = verificationGasLimit;
+//         uint128 maxPriorityFeePerGas = 256;
+//         uint128 maxFeePerGas = maxPriorityFeePerGas;
+
+//         return PackedUserOperation({
+//             sender: sender,
+//             nonce: nonce,
+//             initCode: hex"",
+//             callData: callData,
+//             accountGasLimits: bytes32(uint256(verificationGasLimit) << 128 | callGasLimit),
+//             preVerificationGas: verificationGasLimit,
+//             gasFees: bytes32(uint256(maxPriorityFeePerGas) << 128 | maxFeePerGas),
+//             paymasterAndData: hex"",
+//             signature: hex""
+//         });
+//     }
+// }
--- test/ethereum/MinimalAccountTest.t.sol
+++ test/ethereum/MinimalAccountTest.t.sol
@@ -1,126 +1,123 @@
-//SPDX-License-Identifier: MIT
-pragma solidity ^0.8.28;
+// //SPDX-License-Identifier: MIT
+// pragma solidity ^0.8.28;
 
-import {Test} from "forge-std/Test.sol";
-import {HelperConfig} from "script/HelperConfig.s.sol";
-import {DeployMinimal} from "script/DeployMinimal.s.sol";
-import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
-import {SendPackedUserOp, PackedUserOperation,IEntryPoint} from "script/SendPackedUserOp.s.sol";
-import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
-import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
-import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
+// import {Test} from "forge-std/Test.sol";
+// import {HelperConfig} from "script/HelperConfig.s.sol";
+// import {DeployMinimal} from "script/DeployMinimal.s.sol";
+// import {MinimalAccount} from "src/ethereum/MinimalAccount.sol";
+// import {SendPackedUserOp, PackedUserOperation,IEntryPoint} from "script/SendPackedUserOp.s.sol";
+// import {ECDSA} from "lib/openzeppelin-contracts/contracts/utils/cryptography/ECDSA.sol";
+// import {ERC20Mock} from "lib/openzeppelin-contracts/contracts/mocks/token/ERC20Mock.sol";
+// import {MessageHashUtils} from "lib/openzeppelin-contracts/contracts/utils/cryptography/MessageHashUtils.sol";
 
-contract MinimalAccountTest is Test {
-    using MessageHashUtils for bytes32;
-    HelperConfig helperConfig;
-    MinimalAccount minimalAccount;
-    ERC20Mock usdc;
-    SendPackedUserOp sendPackedUserOp;
+// contract MinimalAccountTest is Test {
+//     using MessageHashUtils for bytes32;
+//     HelperConfig helperConfig;
+//     MinimalAccount minimalAccount;
+//     ERC20Mock usdc;
+//     SendPackedUserOp sendPackedUserOp;
 
-    address randomUser = makeAddr("user");
+//     address randomUser = makeAddr("user");
 
-    uint256 constant AMOUNT = 1e18;
+//     uint256 constant AMOUNT = 1e18;
 
-    function setUp() public {
-        DeployMinimal deployMinimal = new DeployMinimal();
-        (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
-        usdc = new ERC20Mock();
-        sendPackedUserOp = new SendPackedUserOp();
-    }
+//     function setUp() public {
+//         DeployMinimal deployMinimal = new DeployMinimal();
+//         (helperConfig, minimalAccount) = deployMinimal.deployMinimalAccount();
+//         usdc = new ERC20Mock();
+//         sendPackedUserOp = new SendPackedUserOp();
+//     }
 
-    function testOwnerCanExecuteCommands() public {
-        //Arrange
-        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
-        address dest = address(usdc);
-        uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
-        //Act
-        vm.prank(minimalAccount.owner());
-        minimalAccount.execute(dest, value, functionData);
-        //Assert
-        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
-    }
+//     function testOwnerCanExecuteCommands() public {
+//         //Arrange
+//         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+//         address dest = address(usdc);
+//         uint256 value = 0;
+//         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+//         //Act
+//         vm.prank(minimalAccount.owner());
+//         minimalAccount.execute(dest, value, functionData);
+//         //Assert
+//         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+//     }
 
-    function testNonOwnerCannotExecuteCommands() public {
-        //Arrange
-        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
-        address dest = address(usdc);
-        uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
-        //Act
-        vm.prank(randomUser);
-        vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
-        minimalAccount.execute(dest, value, functionData);
-        //Assert
-        // assertEq(usdc.balanceOf(address(minimalAccount)),AMOUNT);
-    }
+//     function testNonOwnerCannotExecuteCommands() public {
+//         //Arrange
+//         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+//         address dest = address(usdc);
+//         uint256 value = 0;
+//         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+//         //Act
+//         vm.prank(randomUser);
+//         vm.expectRevert(MinimalAccount.MinimalAccount__NotFromEntryPointOrOwner.selector);
+//         minimalAccount.execute(dest, value, functionData);
+//         //Assert
+//         // assertEq(usdc.balanceOf(address(minimalAccount)),AMOUNT);
+//     }
 
-    function testRecoverSignedOp() public  {
-        //arrange 
-        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
-        address dest = address(usdc);
-        uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
-        bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector,dest,value,functionData);
-        PackedUserOperation memory packedUserOp = sendPackedUserOp.generatedSignedUserOperation(executeCallData,helperConfig.getConfig(),address(minimalAccount));
-        bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
-        //act 
-        address actualSigner = ECDSA.recover(userOperationHash.toEthSignedMessageHash(),packedUserOp.signature);
+//     function testRecoverSignedOp() public  {
+//         //arrange 
+//         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+//         address dest = address(usdc);
+//         uint256 value = 0;
+//         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+//         bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector,dest,value,functionData);
+//         PackedUserOperation memory packedUserOp = sendPackedUserOp.generatedSignedUserOperation(executeCallData,helperConfig.getConfig(),address(minimalAccount));
+//         bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+//         //act 
+//         address actualSigner = ECDSA.recover(userOperationHash.toEthSignedMessageHash(),packedUserOp.signature);
         
-        //assert 20:00 
-        assertEq(actualSigner,minimalAccount.owner());
-    }
+//         //assert 20:00 
+//         assertEq(actualSigner,minimalAccount.owner());
+//     }
 
-    //1 SIGN USER OPS
-    //2 CALL VALIDATE USRE OPS
-    //3 ASSERT THE RETURN IS CORRECT
-    function testValidationOfUserOps() public{
-        //arrange 
-        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
-        address dest = address(usdc);
-        uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
-        bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector,dest,value,functionData);
-        PackedUserOperation memory packedUserOp = sendPackedUserOp.generatedSignedUserOperation(executeCallData,helperConfig.getConfig(),address(minimalAccount));
-        bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
-        uint256 missingAccountFunds = 1e18;
+//     //1 SIGN USER OPS
+//     //2 CALL VALIDATE USRE OPS
+//     //3 ASSERT THE RETURN IS CORRECT
+//     function testValidationOfUserOps() public{
+//         //arrange 
+//         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+//         address dest = address(usdc);
+//         uint256 value = 0;
+//         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+//         bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector,dest,value,functionData);
+//         PackedUserOperation memory packedUserOp = sendPackedUserOp.generatedSignedUserOperation(executeCallData,helperConfig.getConfig(),address(minimalAccount));
+//         bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+//         uint256 missingAccountFunds = 1e18;
 
-        vm.prank(helperConfig.getConfig().entryPoint);
-        uint256 validationData = minimalAccount.validateUserOp(packedUserOp,userOperationHash,missingAccountFunds);
+//         vm.prank(helperConfig.getConfig().entryPoint);
+//         uint256 validationData = minimalAccount.validateUserOp(packedUserOp,userOperationHash,missingAccountFunds);
 
-        //act 
-        assertEq(validationData,0);
-    }
+//         //act 
+//         assertEq(validationData,0);
+//     }
 
-    function testEntryPointCanExecuteCommands() public {
-        //arrange 
-        assertEq(usdc.balanceOf(address(minimalAccount)), 0);
-        address dest = address(usdc);
-        uint256 value = 0;
-        bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
-        bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector,dest,value,functionData);
-        PackedUserOperation memory packedUserOp = sendPackedUserOp.generatedSignedUserOperation(executeCallData,helperConfig.getConfig(),address(minimalAccount));
-        // bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
+//     function testEntryPointCanExecuteCommands() public {
+//         //arrange 
+//         assertEq(usdc.balanceOf(address(minimalAccount)), 0);
+//         address dest = address(usdc);
+//         uint256 value = 0;
+//         bytes memory functionData = abi.encodeWithSelector(ERC20Mock.mint.selector, address(minimalAccount), AMOUNT);
+//         bytes memory executeCallData = abi.encodeWithSelector(MinimalAccount.execute.selector,dest,value,functionData);
+//         PackedUserOperation memory packedUserOp = sendPackedUserOp.generatedSignedUserOperation(executeCallData,helperConfig.getConfig(),address(minimalAccount));
+//         // bytes32 userOperationHash = IEntryPoint(helperConfig.getConfig().entryPoint).getUserOpHash(packedUserOp);
 
-        vm.deal(address(minimalAccount),1e18);
+//         vm.deal(address(minimalAccount),1e18);
 
-        PackedUserOperation[] memory ops = new PackedUserOperation[](1);
-        ops[0] = packedUserOp;
+//         PackedUserOperation[] memory ops = new PackedUserOperation[](1);
+//         ops[0] = packedUserOp;
 
 
-        //act
-        // vm.startPrank(randomUser);
-        // IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(ops,payable(randomUser));
-        address defaultTxOriginAddress = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
+//         //act
+//         // vm.startPrank(randomUser);
+//         // IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(ops,payable(randomUser));
+//         address defaultTxOriginAddress = 0x1804c8AB1F12E6bbf3894d4083f33e07309d1f38;
 
-        vm.startPrank(randomUser);
-        IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(ops, payable(defaultTxOriginAddress));
-        vm.stopPrank();
+//         vm.startPrank(randomUser);
+//         IEntryPoint(helperConfig.getConfig().entryPoint).handleOps(ops, payable(defaultTxOriginAddress));
+//         vm.stopPrank();
 
-        //assert
-        assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
-    }
-    function testMeowGoesTheMoon() public {
-    }
-        
-}
+//         //assert
+//         assertEq(usdc.balanceOf(address(minimalAccount)), AMOUNT);
+//     }        
+// }
